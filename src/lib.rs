fa0f6eb135b07a3cd22060c12bf3d6d041e2992c
Alex <47791944+AlexSherbinin@users.noreply.github.com>
2022-07-17 18:18:28 +0000

    Documentating

--- src/lib.rs
+++ src/lib.rs
@@ -4,10 +4,11 @@ pub trait PushFront {
 }
 
 impl PushFront for String {
+    /// Appends the given char to the front of this String
     fn push_front(&mut self, ch: char) {
         self.insert(0, ch);
     }
-
+    /// Appends a given string slice onto the front of this String
     fn push_front_str(&mut self, string: &str) {
         self.insert_str(0, string);
     }
